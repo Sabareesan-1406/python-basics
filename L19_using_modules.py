597ab56958211253298dc908c6769c1ee050b0bb
sabar <sabareesan.k.m@gmail.com>
2023-09-15 18:27:29 +0530

    added new comment in the file

--- L19_using_modules.py
+++ L19_using_modules.py
@@ -1,4 +1,5 @@
 from my_module import simple_module
+# python git demo
 
 # here we are using a function we created in the module my_module under the file simple_module
 simple_module.say_hi()
---
+++ L20_functions_demo.py
@@ -0,0 +1,7 @@
+def multiply(a,b):
+    return a*b
+x = multiply(2,9)
+print (x)
+
+
+
